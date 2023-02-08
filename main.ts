a2300d52f5362c6525e70a3cf040824f66ea34bf
erchurumbedemagala <124789483+erchurumbedemagala@users.noreply.github.com>
2023-02-08 13:58:42 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    numero_introducido += 10
+})
+input.onGesture(Gesture.Shake, function () {
+    numero_introducido = 0
+    numero_a_adivinar = randint(1, 99)
+})
+input.onButtonPressed(Button.B, function () {
+    numero_introducido += 1
+})
+let numero_a_adivinar = 0
 let numero_introducido = 0
+numero_introducido = 0
 basic.showString("" + (numero_introducido))
 basic.forever(function () {
-	
+    basic.showString("" + (numero_introducido))
 })
